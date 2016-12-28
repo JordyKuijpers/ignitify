b3825bb253076dd3c7f8e8529a9e70b240151315
Jordy Kuijpers <info@jordykuijpers.com>
2016-12-29 00:08:43 +0100

    REST Controllers now allow Cross Origin requests from localhost:8002. Also, index.html now holds track info and playing queue info!

--- backend/ShpotifyBashWrapper/src/main/java/com/jordykuijpers/spotify/rest/CurrentTrackController.java
+++ backend/ShpotifyBashWrapper/src/main/java/com/jordykuijpers/spotify/rest/CurrentTrackController.java
@@ -1,6 +1,7 @@
 package com.jordykuijpers.spotify.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ public class CurrentTrackController {
 	SpotifyPlayer spotifyPlayer;
 	
 	@RequestMapping("/CurrentTrack")
+	@CrossOrigin(origins = "http://localhost:8002")
 	public CurrentTrack getCurrentTrack() {
 		CurrentTrack ct = new CurrentTrack();
 		ct.setTrack(spotifyPlayer.getCurrentTrack());
--- backend/ShpotifyBashWrapper/src/main/java/com/jordykuijpers/spotify/rest/PlayingQueueController.java
+++ backend/ShpotifyBashWrapper/src/main/java/com/jordykuijpers/spotify/rest/PlayingQueueController.java
@@ -3,27 +3,13 @@ package com.jordykuijpers.spotify.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.google.common.collect.Lists;
 import com.jordykuijpers.spotify.SpotifyPlayer.ISpotifyPlayable;
 import com.jordykuijpers.spotify.SpotifyPlayer.SpotifyPlayer;
-
-/*
-@RestController
-public class GreetingController {
-
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
-
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
-    }
-}
- */
 
 @RestController
 public class PlayingQueueController {
@@ -32,6 +18,7 @@ public class PlayingQueueController {
 	SpotifyPlayer spotifyPlayer;
 
 	@RequestMapping("/PlayingQueue")
+	@CrossOrigin(origins = "http://localhost:8002")
 	public PlayingQueue getPlayingQueue() {
 		List<ISpotifyPlayable> queueTrackList = Lists.newArrayList(spotifyPlayer.playingQueue.iterator());
 		
